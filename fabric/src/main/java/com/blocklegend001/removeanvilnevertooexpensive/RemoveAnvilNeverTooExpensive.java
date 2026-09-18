3c7260fa73249996ace49ac71543a430de942c93
BlockLegend001 <rosarioarcamone@gmail.com>
2026-09-18 12:26:17 +0200

    Added 26.2

---
+++ fabric/src/main/java/com/blocklegend001/removeanvilnevertooexpensive/RemoveAnvilNeverTooExpensive.java
@@ -0,0 +1,11 @@
+package com.blocklegend001.removeanvilnevertooexpensive;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RemoveAnvilNeverTooExpensive implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        CommonClass.init();
+    }
+}
